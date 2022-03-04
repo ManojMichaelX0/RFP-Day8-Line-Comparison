4201560d2f24e42c55786413c47cd900e3a6168a
ManojMichaelX0 <98898332+ManojMichaelX0@users.noreply.github.com>
2022-03-04 16:50:33 +0530

    Update Program.cs

--- UseCase-4-day8-LineComparison/UseCase-4-day8-LineComparison/Program.cs
+++ UseCase-4-day8-LineComparison/UseCase-4-day8-LineComparison/Program.cs
@@ -1,18 +1,21 @@
-﻿using System;
+using System;
 
 namespace UseCase_4_LineComparision
 {
-    public class UceCase_4_line
+    public class Line_Comparison
     {
-        interface Line
+        //variables
+        public double length_Of_Line;
+
+        public interface ILine
         {
             public void Calc();
         }
+
+       
     }
-    public class Program : UceCase_4_line
+    public class Program : Line_Comparison
     {
-        //variables
-        double length_Of_Line;
 
         //method for calculation
         public void Calc(double x1, double y1, double x2, double y2)
@@ -24,14 +27,14 @@ namespace UseCase_4_LineComparision
         public static void Main(String[] args)
         {
             // creating object 1 and reading input from user
-            Program p1 = new Program();
+            Program p = new Program();
             Console.WriteLine("enter line 1 values x1,y1,x2,y2 values\n");
             double a = Convert.ToDouble(Console.ReadLine());
             double b = Convert.ToDouble(Console.ReadLine());
             double c = Convert.ToDouble(Console.ReadLine());
             double d = Convert.ToDouble(Console.ReadLine());
-            p1.Calc(a, b, c, d);
-            Console.WriteLine("Line1 Length is = " + p1.length_Of_Line);
+            p.Calc(a, b, c, d);
+            Console.WriteLine("Line1 Length is = " + p.length_Of_Line);
 
             // creating object 2 and reading input from user
             Program p2 = new Program();
@@ -44,13 +47,11 @@ namespace UseCase_4_LineComparision
             Console.WriteLine("Line 2 Length is = " + p2.length_Of_Line);
             //comparing both line values to if its equal Returns True if equal else False
             Console.WriteLine("Checking lines are equal the answer is");
-            Console.WriteLine((p1.length_Of_Line).Equals(p2.length_Of_Line));
+            Console.WriteLine((p.length_Of_Line).Equals(p2.length_Of_Line));
             //comparing both line values to find line 1 is greater than line 2 Returns "1" if line 1 is greater then line, if line 1 is less than line 2 "-1"
             Console.WriteLine("Compairing line 1 to line 2 \nif returned 1 :line 1 is greater than line 2\nif returned -1 :line 1 is less than line 2");
-            Console.WriteLine((p1.length_Of_Line).CompareTo(p2.length_Of_Line));
+            Console.WriteLine((p.length_Of_Line).CompareTo(p2.length_Of_Line));
         }
 
     }
 }
-
-
